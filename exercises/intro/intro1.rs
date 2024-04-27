537aa17ed004d23e1bf04bf153691fff5e7dd9a8
wjx <13893319248@163.com>
2024-04-27 12:28:31 +0800

    update

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -15,6 +15,7 @@
 
 
 fn main() {
+    println!("wjx is here");
     println!("Hello and");
     println!(r#"       welcome to...                      "#);
     println!(r#"                 _   _ _                  "#);
