4b359eb5f0b1a6730ec975c11ad542a9942594c0
wjx <13893319248@163.com>
2024-04-27 14:04:26 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -20,6 +20,14 @@
 
 // Don't modify this function!
 #[test]
+fn calculate_price_of_apples(number:i32)->i32{
+    if number > 40 {
+        number
+    }
+    else{
+        number*2
+    }
+}
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
     let price2 = calculate_price_of_apples(40);
