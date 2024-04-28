66420565d2d0fc8efae7a9483e36bea02b394d48
wjx <13893319248@163.com>
2024-04-28 10:59:13 +0800

    update

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -8,10 +8,10 @@
 #[derive(Debug)]
 enum Message {
     // TODO: define the different variants used below
-    Quit,
-    Echo(String),
     Move{x:i32,y:i32},
-    ChangeColor{i32,i32,i32},
+    Echo(String),
+    ChangeColor(i32,i32,i32),
+    Quit,
 }
 
 impl Message {
